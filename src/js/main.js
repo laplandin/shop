a633940a650f0b91717e5183bd0cfc119e9f5116
laplandin <deniskakhv@gmail.com>
2017-01-26 23:22:45 +0300

    section to page bottom, accordion added, grid finished!

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
+// slider initialization
   $('.flexslider').flexslider({
     animation: "slide"
   });
+// slider end
+// accordion goes here
+    jQuery(function() {
+      jQuery('.ss_button').on('click',function() {
+        if (jQuery(this).next('.ss_content').is(':visible')) {
+          jQuery('.ss_content').slideUp('fast');
+        } else {
+          jQuery('.ss_content').slideUp('fast');
+          jQuery(this).next('.ss_content').slideDown('fast');
+        }
+      });
+    });
+//accordion end
 });
